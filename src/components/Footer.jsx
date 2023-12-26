1c3b0103d48cd9fe33146f07d66f59e537c2a38e
JiteshBalani <balani.jitesh911@gmail.com>
2023-12-26 18:55:19 +0530

    footer, navbar and index.html files edited for changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,16 @@ const Footer = () => {
 
     const currentYear = new Date().getFullYear();
 
+    // Footer section 1 = (Logo + copyright) + Company
+    // Footer section 2 = (Contact us + Legal) + We deliver to
+
     return (
         <div
             className='flex bg-black text-white px-[200px] py-[25px] justify-around'
         >
+            {/* SECTION 1 STARTS HERE. */}
+
+            {/* Copyright +Company section starts here. */}
             <div className='flex justify-between space-x-32'>
                 <div className='flex flex-col space-y-[6px]'>
                     <div className='flex items-center space-x-2'>
@@ -29,7 +35,14 @@ const Footer = () => {
                     <div className='cursor-pointer'>YT Genie</div>
                 </div>
             </div>
+            {/* ========================================================================== Copyright +Company section starts here. */}
+            {/* SECTION 1 ENDS HERE. */}
+
+
+            {/* SECTION 2 STARTS HERE. */}
             <div className='flex justify-between space-x-32'>
+
+                {/* Contact us + Legal sections starts here. */}
                 <div className='space-y-14'>
                     <div className='contact-us text-gray-400 space-y-[6px]'>
                         <div className='text-[#F05455] text-xl font-semibold'>Contact Us</div>
@@ -44,6 +57,9 @@ const Footer = () => {
                         <div className='cursor-pointer'>Privacy Policy</div>
                     </div>
                 </div>
+                {/* ===================================================================== Contact us + Legal sections starts here. */}
+
+                {/* We deliver to section starts here */}
                 <div>
                     <div className='we-deliver-to text-gray-400 space-y-[6px]'>
                         <div className='text-[#F05455] text-xl font-semibold'>We deliver to</div>
@@ -55,7 +71,11 @@ const Footer = () => {
                         <div className='cursor-pointer'>Pune</div>
                     </div>
                 </div>
+                {/* =========================================================================== We deliver to section ends here */}
+
             </div>
+            {/* SECTION 2 STARTS HERE. */}
+
 
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,42 @@
 const Navbar = () => {
   return (
-    <div className='px-[200px] py-2 text-2xl flex justify-between items-center border-b-2'>
-        <img 
-        className="w-[70px] h-[70px]"
-        alt="brand_logo" 
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0ZOX5_X_GNmsVPTdmVIBwbt2yNz9rPihllg&usqp=CAU"></img>
-        <div className="flex space-x-8 pr-5">
-            <div>Home</div>
-            <div>Offers</div>
-            <div>Help</div>
-            <div><img
-                  className="w-[40px] h-[40px]"
-                  alt="cart_icon"
-                  src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png"
-                ></img>
-            </div>
+    <div className='px-[200px] py-2 text-xl flex justify-between items-center border-b-2'>
+      <div className="flex items-center space-x-1">
+        <img
+          className="w-[60px] h-[60px] rounded-md"
+          alt="brand_logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0ZOX5_X_GNmsVPTdmVIBwbt2yNz9rPihllg&usqp=CAU"></img>
+        <div className="text-3xl font-semibold text-[#F05455]">Yummy Tummy</div>
+      </div>
+      <div className="flex space-x-8 pr-5 text-[#F05455] font-semibold">
+        <div className="flex items-center space-x-1">
+          <img
+            alt="offer-icon"
+            className="w-[20px] h-[20px]"
+            src="https://cdn-icons-png.flaticon.com/512/3508/3508366.png"></img>
+          <div className="cursor-pointer">Home</div>
         </div>
+        <div className="flex items-center space-x-1">
+          <img
+            alt="offer-icon"
+            className="w-[20px] h-[20px]"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOq9jCIZLTV9PhvgX_Y-oj6MUjba9Vm37q2g&usqp=CAU"></img>
+          <div className="cursor-pointer">Offers</div>
+        </div>
+        <div className="flex items-center space-x-1">
+          <img
+            alt="offer-icon"
+            className="w-[20px] h-[20px]"
+            src="https://static.thenounproject.com/png/2643287-200.png"></img>
+          <div className="cursor-pointer">Help</div>
+        </div>
+        <div><img
+          className="w-[40px] h-[40px] cursor-pointer"
+          alt="cart_icon"
+          src="https://www.iconpacks.net/icons/2/free-shopping-cart-icon-2029-thumb.png"
+        ></img>
+        </div>
+      </div>
     </div>
   )
 }
