0d7b89ed14425fbe32f3f5a500715714b19f390c
JiteshBalani <balani.jitesh911@gmail.com>
2023-12-28 00:48:27 +0530

    added costForTwo attribute in RestaturantCard

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -9,22 +9,25 @@ const RestaurantCard = (props) => {
         avgRating,
         sla: { deliveryTime },
         cuisines,
+        costForTwo
     } = resData?.info;
     return (
         <div
-            className='flex flex-col items-center w-64 rounded-md space-y-1'
+            className='flex flex-col items-center w-64 rounded-md space-y-1 cursor-pointer '
         >
             <div><img
                 className='h-[180px] w-screen rounded-md bg-center bg-cover'
                 alt='restaurant_image'
                 src={resBanner + cloudinaryImageId}
             ></img></div>
-            <div className='text-xl font-semibold'>{name}</div>
+            <div className='text-xl font-semibold text-center w-full truncate'>{name}</div>
             <div className='flex text-lg font-semibold space-x-3'>
                 <div className='flex '><h2 className='text-green-600'>&#9734;</h2> {avgRating}</div>
                 <div>~{deliveryTime} mins</div>
             </div>
-            <div className='text-md font-thin'>{cuisines.join(", ")}</div>
+            <div className='text-md text-center font-thin truncate w-full'>{cuisines.join(", ")}</div>
+            <div className="text-md text-center text-[#F05455] font-semibold">{costForTwo}</div>
+
 
         </div>
     )
