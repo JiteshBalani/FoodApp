82190cfd1833789ca88e1625e16eef008b01b2dd
JiteshBalani <balani.jitesh911@gmail.com>
2023-12-27 19:43:55 +0530

    utils folder created

---
+++ src/utils/cardData.js
@@ -0,0 +1,645 @@
+export const restaurants = [
+    {
+    "info": {
+    "id": "673438",
+    "name": "Kwality Walls Frozen Dessert and Ice Cream Shop",
+    "cloudinaryImageId": "909daf097e30cd01d8fd121ddc6de2f6",
+    "locality": "Radha Nagari",
+    "areaName": "Kalyan",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "Desserts",
+    "Ice Cream",
+    "Ice Cream Cakes"
+    ],
+    "avgRating": 4.3,
+    "veg": true,
+    "parentId": "582",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "100+",
+    "sla": {
+    "deliveryTime": 13,
+    "lastMileTravel": 1.7,
+    "serviceability": "SERVICEABLE",
+    "slaString": "13 mins",
+    "lastMileTravelString": "1.7 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-01-03 00:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "50% OFF",
+    "subHeader": "UPTO ₹100"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-39010c87-e463-47da-af8f-f4d695ff6975"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/kwality-walls-frozen-dessert-and-ice-cream-shop-radha-nagari-kalyan-mumbai-673438",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "326588",
+    "name": "Nothing But Chicken",
+    "cloudinaryImageId": "mgtawa7tgpwob9tsigle",
+    "locality": "Char Rasta",
+    "areaName": "Dombivli East",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Salads",
+    "Biryani",
+    "Kebabs",
+    "Fast Food",
+    "Healthy Food"
+    ],
+    "avgRating": 4.3,
+    "parentId": "668",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 29,
+    "lastMileTravel": 8.4,
+    "serviceability": "SERVICEABLE",
+    "slaString": "29 mins",
+    "lastMileTravelString": "8.4 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-27 05:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {},
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-39010c87-e463-47da-af8f-f4d695ff6975"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/nothing-but-chicken-char-rasta-dombivli-east-mumbai-326588",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "606083",
+    "name": "My Family Resturant",
+    "cloudinaryImageId": "w98lvqopingyela6rtak",
+    "locality": "Apmc Bhaji Market",
+    "areaName": "Kalyan",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "South Indian",
+    "North Indian",
+    "Chinese",
+    "Pizzas",
+    "Beverages"
+    ],
+    "avgRating": 3.8,
+    "veg": true,
+    "parentId": "373929",
+    "avgRatingString": "3.8",
+    "totalRatingsString": "500+",
+    "sla": {
+    "deliveryTime": 27,
+    "lastMileTravel": 3.8,
+    "serviceability": "SERVICEABLE",
+    "slaString": "27 mins",
+    "lastMileTravelString": "3.8 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-27 16:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "10% OFF",
+    "subHeader": "UPTO ₹40"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-39010c87-e463-47da-af8f-f4d695ff6975"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/my-family-resturant-apmc-bhaji-market-kalyan-mumbai-606083",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "76757",
+    "name": "Hotel koliwada",
+    "cloudinaryImageId": "iqsnfvzedheo65dv925h",
+    "locality": "Near Rustom Petrol Pump",
+    "areaName": "Kalyan",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "North Indian",
+    "Chinese"
+    ],
+    "avgRating": 3.5,
+    "parentId": "99821",
+    "avgRatingString": "3.5",
+    "totalRatingsString": "100+",
+    "sla": {
+    "deliveryTime": 20,
+    "lastMileTravel": 1.2,
+    "serviceability": "SERVICEABLE",
+    "slaString": "20 mins",
+    "lastMileTravelString": "1.2 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-27 05:30:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "10% OFF",
+    "subHeader": "UPTO ₹100"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-39010c87-e463-47da-af8f-f4d695ff6975"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/hotel-koliwada-near-rustom-petrol-pump-kalyan-mumbai-76757",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "402502",
+    "name": "Smoky Jone's",
+    "cloudinaryImageId": "ggbnukk8l0bfgsyibs4b",
+    "areaName": "Kalyan",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Chinese",
+    "Thai",
+    "Oriental"
+    ],
+    "avgRating": 3.7,
+    "parentId": "255652",
+    "avgRatingString": "3.7",
+    "totalRatingsString": "100+",
+    "sla": {
+    "deliveryTime": 25,
+    "lastMileTravel": 1.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25 mins",
+    "lastMileTravelString": "1.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-27 05:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {},
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-39010c87-e463-47da-af8f-f4d695ff6975"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/smoky-jones-kalyan-mumbai-402502",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "710556",
+    "name": "Cilantro",
+    "cloudinaryImageId": "375f8d233613b205ab37f42d110edc88",
+    "locality": "Chikan Ghar Rd",
+    "areaName": "Kalyan",
+    "costForTwo": "₹398 for two",
+    "cuisines": [
+    "North Indian",
+    "Chinese",
+    "Biryani"
+    ],
+    "avgRating": 3.9,
+    "parentId": "3900",
+    "avgRatingString": "3.9",
+    "totalRatingsString": "20+",
+    "sla": {
+    "deliveryTime": 21,
+    "lastMileTravel": 1.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "21 mins",
+    "lastMileTravelString": "1.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-27 05:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {},
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-39010c87-e463-47da-af8f-f4d695ff6975"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/cilantro-chikan-ghar-rd-kalyan-mumbai-710556",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "702517",
+    "name": "Forest Momo",
+    "cloudinaryImageId": "1cfddf77b72eb9ea2dacf29c2b68481b",
+    "locality": "Shastri Nagar",
+    "areaName": "Dombivli",
+    "costForTwo": "₹220 for two",
+    "cuisines": [
+    "Chinese"
+    ],
+    "avgRating": 4.3,
+    "parentId": "419560",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "50+",
+    "sla": {
+    "deliveryTime": 38,
+    "lastMileTravel": 9.2,
+    "serviceability": "SERVICEABLE",
+    "slaString": "38 mins",
+    "lastMileTravelString": "9.2 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-27 23:58:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {},
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-39010c87-e463-47da-af8f-f4d695ff6975"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/forest-momo-shastri-nagar-dombivli-mumbai-702517",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "677929",
+    "name": "Sivaji Idli",
+    "cloudinaryImageId": "ca24677cec294c447db7ed3ddd40c124",
+    "locality": "Shastri Nagar",
+    "areaName": "Dombivli",
+    "costForTwo": "₹140 for two",
+    "cuisines": [
+    "South Indian",
+    "Beverages"
+    ],
+    "avgRating": 4,
+    "veg": true,
+    "parentId": "405241",
+    "avgRatingString": "4.0",
+    "totalRatingsString": "50+",
+    "sla": {
+    "deliveryTime": 38,
+    "lastMileTravel": 9.2,
+    "serviceability": "SERVICEABLE",
+    "slaString": "38 mins",
+    "lastMileTravelString": "9.2 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-27 23:59:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {},
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-39010c87-e463-47da-af8f-f4d695ff6975"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/sivaji-idli-shastri-nagar-dombivli-mumbai-677929",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "703532",
+    "name": "Hirakani Snacks Corner",
+    "cloudinaryImageId": "499a997a1d4c85c9c0d9a77551c8429f",
+    "locality": "Wadeghar Main Rd",
+    "areaName": "Kalyan",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "Chinese",
+    "Snacks",
+    "Pizzas"
+    ],
+    "avgRating": 2.8,
+    "parentId": "420109",
+    "avgRatingString": "2.8",
+    "totalRatingsString": "50+",
+    "sla": {
+    "deliveryTime": 29,
+    "lastMileTravel": 3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "29 mins",
+    "lastMileTravelString": "3.0 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-27 05:30:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "30% OFF",
+    "subHeader": "UPTO ₹150",
+    "discountTag": "SAVE BIG"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-39010c87-e463-47da-af8f-f4d695ff6975"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/hirakani-snacks-corner-wadeghar-main-rd-kalyan-mumbai-703532",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+    ]
